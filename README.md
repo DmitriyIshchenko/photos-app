d1488397370e6509f3b3316687720e1bd17e42bf
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-01-22 10:05:54 +0300

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple photos app
+
+Click [here](https://github.com/avito-tech/safedeal-frontend-trainee) to see details.
---
+++ src/features/photos/photosSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const photosSlice = createSlice({
+    name: "photos",
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default photosSlice.reducer;
