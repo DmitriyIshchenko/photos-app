adc66e17ce3941040c835604e261f853f661ea85
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-01-24 16:50:13 +0300

    Add header and footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import PhotosList from './features/photos/PhotosList';
 function App() {
   return (
     <div className="App">
+      <header><h1>Photos App</h1></header>
       <PhotosList />
+      <footer>&copy; 2022</footer>
     </div>
   );
 }
